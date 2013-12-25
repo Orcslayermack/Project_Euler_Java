507470b6203e200d6a2e15c712e9f4c97061dad1
orcslayermack@gmail.com <dTDkpPNTDG8jPCiPCGRN>
2013-12-25 01:32:07 -0700

    Started Project 07

---
+++ src/project07/Project07.java
@@ -0,0 +1,24 @@
+package project07;
+
+// By listing the first six prime numbers: 2, 3, 5, 7, 11, 
+// and 13, we can see that the 6th prime is 13.
+
+// What is the 10,001st prime number?
+
+
+public class Project07 {
+
+	public static void main(String[] args) {
+		// TODO make a list of the first 10,001 prime numbers.
+		// I'm going to try to use the sieve of aristophanes.
+		
+		
+		
+		
+		
+		
+		// TODO print the last one.
+
+	}
+
+}
